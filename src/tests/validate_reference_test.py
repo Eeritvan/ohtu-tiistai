cf988795de3313de08279d694b56fbfa75d38d1b
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-13 22:49:06 +0200

    Fixed unit tests more.

--- src/tests/validate_reference_test.py
+++ src/tests/validate_reference_test.py
@@ -6,12 +6,14 @@ class TestReferenceValidation(unittest.TestCase):
         pass
 
     def test_valid_length_does_not_raise_error(self):
-        validate_reference("juokse", "juokse", "juokse", "juokse", "juokse")
-        validate_reference("a" * 100, "a" * 100, "a" * 100, "a" * 100)
+        """All validated fields are filled and passed"""
+        validate_reference("juokse", "juokse", "juokse", "2020", "juokse")
+        validate_reference("a" * 100, "a" * 100, "a" * 100, "1999", "a" * 100)
 
     def test_too_short_or_long_raises_error(self):
+        """All validated fields are filled with too short or long strings"""
         with self.assertRaises(UserInputError):
             validate_reference("ole", "ole", "ole", "ole", "ole")
 
         with self.assertRaises(UserInputError):
-            validate_reference("koodaa" * 20, "koodaa" * 20, "koodaa" * 20, "koodaa" * 20, "koodaa" * 20)
+            validate_reference("koodaa" * 20, "koodaa" * 20, "koodaa" * 20, "1999", "koodaa" * 20)
