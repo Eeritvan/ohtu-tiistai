acd3c6703a22074fe22b24a10bd232fb6945dd2b
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-13 22:17:08 +0200

    Removed extra forgotten lines.

--- src/util.py
+++ src/util.py
@@ -3,7 +3,7 @@ import re
 class UserInputError(Exception):
     pass
 
-#TODO this needs to be fixed so input is less params and more flexible
+#TODO this needs to be fixed, so input is less params and more flexible for other reference types
 def validate_reference(author, title, booktitle, year, editor, volume, number, series, pages, address, month, organisation, publisher):
     fields_validation_length = {"author": author, "title": title, "book title": booktitle}
 
@@ -36,10 +36,4 @@ def validate_reference(author, title, booktitle, year, editor, volume, number, s
 
     if re.search(r'\b(?:14\d{2}|15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2})\b', year) is None:
           raise UserInputError("Reference year must be a four-digit number.")
-    
-    if len(editor)>0:
-        if len(content) < 5:
-            raise UserInputError(f"Reference {key} length must be greater than 4")
 
-        if len(content) > 100:
-            raise UserInputError(f"Reference {key} length must be smaller than 100")
