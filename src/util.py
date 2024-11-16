9f2a98024301fd224c179f7730e23cab5fbdeeda
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-16 14:09:55 +0200

    Created length validations for form fields and year is validated as 4 digits

--- src/util.py
+++ src/util.py
@@ -3,49 +3,46 @@ import re
 class UserInputError(Exception):
     pass
 
-#TODO this needs to be fixed, so input is less params and more flexible for \\
-#     other reference types
-def validate_reference(author, title, booktitle, year, editor):
-    #volume, number, series, pages, address, month, organisation, publisher
+def validate_reference(validate_set):
 
-    fields_validation_length = {"author": author,
-                                "title": title,
-                                "book title": booktitle}
+    for key, content in validate_set.items():
 
-    if len(editor)>0:
-        fields_validation_length["editor"] = editor
+        if key == "author" and "title" and "booktitle":
+        # Mandatory: author, title, booktitle, year
+            if len(content) < 3:
+                raise UserInputError(
+                    f"Reference {key} length must be greater than 3"
+                )
 
-    # if len(volume)>0:
-    #     fields_validation_length["volume"] = volume
-    # if len(number)>0:
-    #     fields_validation_length["number"] = number
-    # if len(series)>0:
-    #     fields_validation_length["series"] = series
-    # if len(pages)>0:
-    #     fields_validation_length["pages"] = pages
-    # if len(address)>0:
-    #     fields_validation_length["address"] = address
-    # if len(month)>0:
-    #     fields_validation_length["month"] =  month
-    # if len(organisation)>0:
-    #     fields_validation_length["organisation"] =  organisation
-    # if len(publisher)>0:
-    #     fields_validation_length["publisher"] =  publisher
+            if len(content) > 255:
+                raise UserInputError(
+                    f"Reference {key} length must be smaller than 255"
+                )
+        elif(key == "editor" or "address" or "organisation" or "publisher"):
+        # Optional: editor, address, organisation, publisher
+        # More checking needed?
+            if len(content)>0:
+                print(">0")
+                if len(content) < 3:
+                    print("<3")
+                    raise UserInputError(
+                        f"Reference {key} length must be greater than 3"
+                    )
+            if len(content) > 255:
+                raise UserInputError(
+                    f"Reference {key} length must be smaller than 255"
+                )
+        else:
+        # Optional: volume, number, series, pages
+        # More checking needed
+            if len(content) > 255:
+                raise UserInputError(
+                    f"Reference {key} length must be smaller than 255"
+                )
 
-    for key, content in fields_validation_length.items():
+        # Mandatory: year
+        if key == "year":
+            if re.search(
+                r'\b(?:14\d{2}|15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2})\b', content) is None:
+                raise UserInputError("Reference year must be a four-digit number.")
 
-        if len(content) < 5:
-            raise UserInputError(
-                f"Reference {key} length must be greater than 4"
-            )
-
-        if len(content) > 100:
-            raise UserInputError(
-                f"Reference {key} length must be smaller than 100"
-            )
-
-        if re.search(
-            r'\b(?:14\d{2}|15\d{2}|16\d{2}|17\d{2}|18\d{2}|19\d{2}|20\d{2})\b', 
-            year
-        ) is None:
-            raise UserInputError("Reference year must be a four-digit number.")
