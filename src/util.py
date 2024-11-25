69984d02d567cdcbb2d56d284d775c98a555261f
jkuusto <jukka.kuusto@gmail.com>
2024-11-25 10:54:54 +0200

    Refactor for dryer code and move format logic to util.py

--- src/util.py
+++ src/util.py
@@ -74,3 +74,12 @@ def generate_citekey(i):
 
 def raise_error(message):
     raise UserInputError(message)
+
+def format_inproceedings(reference):
+    citekey = reference.citekey if reference.citekey else "None"
+    bibtex_entry = f"@inproceedings{{{citekey},\n"
+    for key, value in reference.field_values.items():
+        if value is not None:
+            bibtex_entry += f"    {key} = {{{value}}},\n"
+    bibtex_entry = bibtex_entry.rstrip(",\n") + "\n}"
+    return bibtex_entry
