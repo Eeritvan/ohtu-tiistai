8086e53084a8dc47192606e89390f1b8811682be
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-27 19:46:29 +0200

    Reference: db_id can be None and update_id method

--- src/entities/reference.py
+++ src/entities/reference.py
@@ -1,19 +1,35 @@
 class Reference:
-    def __init__(self, db_id, ref_type, citekey=None):
+    """Parent class for all reference types. 
+    Stores basic values which are common to all types. 
+        """
+
+    def __init__(self, ref_type, db_id=None, citekey=None):
+        """Constructor which creates new Reference object"""
         self.id = db_id
         self.ref_type = ref_type
         self.citekey = citekey
 
+    def update_id(self, db_id):
+        """Updates the value of database id"""
+        self.id = db_id
+
     def __str__(self):
+        """Creates a string which contains attribute values."""
         return (
             f"id: {self.id}\n"
             f"type: {self.ref_type}\n"
             f"citekey: {self.citekey}\n"
         )
 
+
 class Inproceedings(Reference):
-    def __init__(self, db_id, ref_type, citekey=None, **kwargs):
-        super().__init__(db_id, ref_type, citekey)
+    """Class for Inproceedings reference type"""
+
+    def __init__(self, **kwargs):
+        """Constructor"""
+        super().__init__(kwargs.get('ref_type', None),
+            kwargs.get('db_id', None), kwargs.get('citekey', None))
+
         self.field_values = {
             "author": kwargs.get('author', None),
             "title": kwargs.get('title', None),
@@ -30,11 +46,6 @@ class Inproceedings(Reference):
             "publisher": kwargs.get('publisher', None)
         }
         self.mandatory_fields = ["author", "title", "booktitle", "year"]
-
-    def fields_not_none(self):
-        field_values_filtered = {k: v for k, v in self.field_values.items()
-                                 if v is not None}
-        return field_values_filtered
 
     def __str__(self):
         return (
