a474b5dcc40b75cd10828723a96710ede51cdb5d
Margit Tepponen <margit.tepponen@helsinki.fi>
2024-12-08 19:46:34 +0200

    add new tables to schema

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,7 +1,7 @@
 from sqlalchemy import text
 from config import db, app
 
-TABLE_NAME = "sources"
+TABLE_NAMES = ["sources", "tags", "sources_tags"]
 
 def table_exists(name):
     sql_table_existence = text(
@@ -19,21 +19,23 @@ def table_exists(name):
     return result.fetchall()[0][0]
 
 def reset_db():
-    print(f"Clearing contents from table {TABLE_NAME}")
-    sql = text(f"DELETE FROM {TABLE_NAME}")
-    db.session.execute(sql)
-    db.session.commit()
-
-def setup_db():
-    if table_exists(TABLE_NAME):
-        print(f"Table {TABLE_NAME} exists, dropping")
-        sql = text(f"DROP TABLE {TABLE_NAME}")
+    for TABLE_NAME in TABLE_NAMES:
+        print(f"Clearing contents from table {TABLE_NAME}")
+        sql = text(f"DELETE FROM {TABLE_NAME}")
         db.session.execute(sql)
         db.session.commit()
 
-    print(f"Creating table {TABLE_NAME}")
+def setup_db():
+    for TABLE_NAME in TABLE_NAMES:
+        if table_exists(TABLE_NAME):
+            print(f"Table {TABLE_NAME} exists, dropping")
+            sql = text(f"DROP TABLE {TABLE_NAME} CASCADE")
+            db.session.execute(sql)
+            db.session.commit()
+
+    print(f"Creating table {TABLE_NAMES[0]}")
     sql = text(
-        f"CREATE TABLE {TABLE_NAME} ("
+        f"CREATE TABLE {TABLE_NAMES[0]} ("
         "  id SERIAL PRIMARY KEY, "
         "  citekey VARCHAR(50) UNIQUE NOT NULL, "
         "  ref_type VARCHAR(50) NOT NULL, "
@@ -57,6 +59,30 @@ def setup_db():
 
     db.session.execute(sql)
     db.session.commit()
+
+
+    print(f"Creating table {TABLE_NAMES[1]}")
+    sql = text(
+        f"CREATE TABLE {TABLE_NAMES[1]} ("
+        " id SERIAL PRIMARY KEY, "
+        " tagname VARCHAR(20) UNIQUE, "
+        " color VARCHAR(20)"
+        ")"
+    )
+    db.session.execute(sql)
+    db.session.commit()
+
+    print(f"Creating table {TABLE_NAMES[2]}")
+    sql = text(
+        f"CREATE TABLE {TABLE_NAMES[2]} ("
+        " id SERIAL PRIMARY KEY, "
+        " source_id INTEGER REFERENCES sources ON DELETE CASCADE, "
+        " tag_id INTEGER REFERENCES tags ON DELETE CASCADE"
+        " )"
+    )
+    db.session.execute(sql)
+    db.session.commit()
+
 
 if __name__ == "__main__":
     with app.app_context():
