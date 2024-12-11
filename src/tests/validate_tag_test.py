68bd316a76bb40c77bf36dbeb2b6a918bd0918e4
eeritvan <eetu.ritvanen@helsinki.fi>
2024-12-11 17:24:56 +0200

    Fixed validate tag pytest

--- src/tests/validate_tag_test.py
+++ src/tests/validate_tag_test.py
@@ -1,5 +1,6 @@
 import unittest
 from services.validate_tag import validate_tag, UserInputError
+from entities.tag import Tag
 
 class TestReferenceValidation(unittest.TestCase):
     def setUp(self):
@@ -8,19 +9,28 @@ class TestReferenceValidation(unittest.TestCase):
     def test_correct_tag_does_not_raise_error(self):
         """Tag is valid"""
 
-        test_tag1 = "testtest"
+        test_tag1 = Tag(name = "testtest",
+                        id = 2,
+                        color = '0, 0, 255')
         validate_tag(test_tag1)
 
-        test_tag2 = "testtesttesttest"
+
+        test_tag2 = Tag(name = "testtesttesttest",
+                        id = 2,
+                        color = '0, 0, 255')
         validate_tag(test_tag2)
 
     def test_invalid_tag_raises_error(self):
         """Tag is too long or too short"""
 
         with self.assertRaises(UserInputError):
-            test_tag1 = "test" * 10
+            test_tag1 = Tag(name = "test" * 10,
+                            id = 2,
+                            color = '0, 0, 255')
             validate_tag(test_tag1)
 
         with self.assertRaises(UserInputError):
-            test_tag2 = "a"
+            test_tag2 = Tag(name = "a",
+                            id = 2,
+                            color = '0, 0, 255')
             validate_tag(test_tag2)
